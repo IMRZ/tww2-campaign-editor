812ae33bef5dd11b023b7cb75a2b6a88a548a5d0
Imad Razzak <im.razzak@gmail.com>
2020-11-28 13:24:39 +0100

    fix warning

--- src/assets/flags/index.js
+++ src/assets/flags/index.js
@@ -1,6 +1,6 @@
 const requireContext = require.context('.', true, /^.+\.png$/);
 const flags = requireContext.keys().reduce((accumulator, image) => {
-  const [/* fullMatch */, collection, dirname, filename] = image.match(/^\.\/(.+)\/(.+)\/(.+)\.png$/);
+  const [/* fullMatch */, /* collection */, dirname, filename] = image.match(/^\.\/(.+)\/(.+)\/(.+)\.png$/);
   const key = `ui\\flags\\${dirname}\\${filename}`
 
   accumulator[key] = requireContext(image);
